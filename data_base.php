47cc0c5637b78aa899224098830210c2e613e6ca
lee1988 <leonellee2016@gmail.com>
2022-02-17 17:28:26 -0600

    Configuracion upload o error informacion base datos 17.02.2022 17:28

--- data_base.php
+++ data_base.php
@@ -20,9 +20,101 @@
     $my_register = mysqli_query($my_conection, $my_query);
 
     if($my_register){
-        echo "Information sent successfully";
+        echo "
+            <link rel='stylesheet' href='styles02.css'/>
+
+            <div class='uploadMessage'>
+            
+                <img src='good.png' id='myImage' />
+                <p id='myMessage'>Information sent successfully</p>
+
+            </div>
+
+            <script>
+
+                let image = document.getElementById('myImage');
+                let message = document.getElementById('myMessage');
+
+                if(window.screen.width <= 550){
+                    image.style.width = '300px';
+                    message.style.fontSize = '70px';
+                }else if (window.screen.width >= 1000 && window.screen.width < 1920){
+                    image.style.width = '150px';
+                    message.style.fontSize = '50px';
+                }else if (window.screen.width >= 1920 && window.screen.width < 2560){
+                    image.style.width = '250px';
+                    message.style.fontSize = '75px';
+                }else if (window.screen.width >= 2560 && window.screen.width < 3840){
+                    image.style.width = '300px';
+                    message.style.fontSize = '75px';
+                }else if (window.screen.width >= 3840 && window.screen.width < 7680){
+                    image.style.width = '500px';
+                    message.style.fontSize = '125px';
+                }else if (window.screen.width >= 7680){
+                    image.style.width = '800px';
+                    message.style.fontSize = '200px';
+                }
+            
+                window.addEventListener('resize', ()=>{
+                    if(window.screen.width < 550){
+                        image.style.width = '300px';
+                        message.style.fontSize = '70px';
+                    }else if(window.screen.width >= 550 && window.screen.width < 1000){
+                        image.style.width = '150px';
+                        message.style.fontSize = '30px';
+                    }
+                });
+        
+            </script>
+        ";
     }else{
-        echo "Error, the information was not sent";
+        echo "
+            <link rel='stylesheet' href='styles02.css'/>
+
+            <div class='uploadMessage'>
+            
+                <img src='wrong.png' id='myImage'/>
+                <p id='myMessage'>Error, the information was not sent</p>
+
+            </div>
+
+            <script>
+
+                let image = document.getElementById('myImage');
+                let message = document.getElementById('myMessage');
+
+                if(window.screen.width <= 550){
+                    image.style.width = '300px';
+                    message.style.fontSize = '70px';
+                }else if (window.screen.width >= 1000 && window.screen.width < 1920){
+                    image.style.width = '150px';
+                    message.style.fontSize = '50px';
+                }else if (window.screen.width >= 1920 && window.screen.width < 2560){
+                    image.style.width = '250px';
+                    message.style.fontSize = '75px';
+                }else if (window.screen.width >= 2560 && window.screen.width < 3840){
+                    image.style.width = '300px';
+                    message.style.fontSize = '75px';
+                }else if (window.screen.width >= 3840 && window.screen.width < 7680){
+                    image.style.width = '500px';
+                    message.style.fontSize = '125px';
+                }else if (window.screen.width >= 7680){
+                    image.style.width = '800px';
+                    message.style.fontSize = '200px';
+                }
+            
+                window.addEventListener('resize', ()=>{
+                    if(window.screen.width < 550){
+                        image.style.width = '300px';
+                        message.style.fontSize = '70px';
+                    }else if(window.screen.width >= 550 && window.screen.width < 1000){
+                        image.style.width = '150px';
+                        message.style.fontSize = '30px';
+                    }
+                });
+    
+             </script>
+        ";
     }
 
     mysqli_close($my_conection);
